7ceb1b5ddcac8792001c4929012555556b74c01f
Mihir Kothari <mihir.k25@gmail.com>
2018-10-27 12:45:55 -0500

    basic routes configuration (#4)

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-var express = require("express");
+let express = require('express');
+let path = require('path');
 
-var app = express();
-var PORT = 8080;
+let app = express();
+let PORT = 8080;
+
+let reservations = [];
+let waitlist = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/tables', function(req, res) {
+  res.sendFile(path.join(__dirname + '/tables.html'));
+});
+
+app.get('/reserve', function(req, res) {
+  res.sendFile(path.join(__dirname + '/reserve.html'));
+});
+
+app.post('/makeReservation', function(req, res) {
+  console.log(req.body);
+  if (reservations.length < 5) {
+    reservations.push(req.body);
+  } else if (waitlist.length < 2) {
+    console.log("Added to waitlist");
+    waitlist.push(req.body);
+  } else {
+    console.log("We can't take your reservation at this moment");
+  }
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
